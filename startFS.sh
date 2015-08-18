20629994410cc45bf5096b53c1c440890a3361aa
Doug Dawson <dougnd@gmail.com>
2015-08-18 15:04:59 -0400

    switched to pulse

--- startFS.sh
+++ startFS.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-fluidsynth -C0 -R0 -g$1 -a alsa -o audio.alsa.device=hw:1 clickTrackSounds.sf2
+#fluidsynth -C0 -R0 -d -g$1 -a alsa -o audio.alsa.device=hw:Series clickTrackSounds.sf2
+fluidsynth -C1 -R0 -d -g$1 -a pulseaudio  clickTrackSounds.sf2
 #/usr/share/sounds/sf2/FluidR3_GM.sf2
